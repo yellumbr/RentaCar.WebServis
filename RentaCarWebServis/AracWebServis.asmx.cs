12806e71e515a92f9020bbc6c342c20e6acb9479
Birkan <birkankader@outlook.com>
2019-05-03 12:57:15 +0300

    Tamamlandı

--- RentaCarWebServis/AracWebServis.asmx.cs
+++ RentaCarWebServis/AracWebServis.asmx.cs
@@ -51,45 +51,6 @@ namespace RentaCarWebServis
                 return false;
             }
         }
-        //[WebMethod]
-        //public bool AracKirala(int AracId,string Plaka, string AracAdi, string AracModeli, 
-        //    int GerekenEhliyetYasi, int MinimumYasSiniri, int GunlukKmSiniri, 
-        //    int AracKm, string HavaYastigi, int BagajHacmi, int KoltukSayisi, 
-        //    int GunlukKiraBedeli, 
-        //    string YakitTipi, string VitesTipi, DateTime KiralanmaTarihi, 
-        //    DateTime KiradanDonusTarihi, string AracResmi)
-        //{
-
-        //    try
-        //    {
-        //        Araclar arac = new Araclar()
-        //        {
-        //            AracId = AracId,
-        //            Plaka = Plaka,
-        //            AracAdi = AracAdi,
-        //            AracModeli = AracModeli,
-        //            GerekenEhliyetYasi = GerekenEhliyetYasi,
-        //            MinimumYasSiniri = MinimumYasSiniri,
-        //            GunlukKiraBedeli = GunlukKiraBedeli,
-        //            GunlukKmSiniri = GunlukKmSiniri,
-        //            AracKm = AracKm,
-        //            HavaYastigi = HavaYastigi,
-        //            BagajHacmi = BagajHacmi,
-        //            KoltukSayisi = KoltukSayisi,
-        //            Rezerv = false,
-        //            Kirada = true,
-        //            YakitTipi = YakitTipi,
-        //            VitesTipi = VitesTipi,
-        //            KiradanDonusTarihi = KiradanDonusTarihi,
-        //            KiralanmaTarihi = KiralanmaTarihi
-        //        }
-
-        //    }
-        //    catch (Exception)
-        //    {
-        //        return false;
-        //    }
-        //}
 
         [WebMethod]
         public bool AracGuncelle(Araclar entity)
@@ -109,13 +70,13 @@ namespace RentaCarWebServis
         }
 
         [WebMethod]
-        public bool AracIdSil(int id)
+        public bool AracIdSil(int id,Sirket sirket)
         {
             try
             {
                 using (var business = new AraclarBusiness())
                 {
-                    business.AracIdSil(id);
+                    business.AracIdSil(id,sirket);
                 }
                 return true;
             }
